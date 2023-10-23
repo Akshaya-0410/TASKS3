d800ebc5598858e16fa333e0d68cc025ed1be4d6
Akshaya-0410 <140970046+Akshaya-0410@users.noreply.github.com>
2023-10-23 12:53:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,29 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
 
-  const handleButtonClick = (value) => {
-    if (value === '=') {
-      try {
-        setResult(eval(input));
-      } catch (error) {
-        setResult('Error');
-      }
-    } else if (value === 'C') {
-      setInput('');
-      setResult('');
-    } else {
-      setInput(input + value);
+  useEffect(() => {
+
+    if(text !== ""){
+      console.log("va")
+      console.log(text.split(' '))
+      setCount(text.match(/\S+/g).length)
     }
-  };
+    
+  },[text])
 
   return (
-    <div className="calculator">
-      <h1>CALCULATOR</h1>
-      <div className="display">
-        <input type="text" value={input} readOnly />
-        <div className="result">{result}</div>
-      </div>
-      <div className="buttons">
-        <div className="row">
-          <button onClick={() => handleButtonClick('7')}>7</button>
-          <button onClick={() => handleButtonClick('8')}>8</button>
-          <button onClick={() => handleButtonClick('9')}>9</button>
-          <button onClick={() => handleButtonClick('+')}>+</button>
-        </div>
-        <div className="row">
-          <button onClick={() => handleButtonClick('4')}>4</button>
-          <button onClick={() => handleButtonClick('5')}>5</button>
-          <button onClick={() => handleButtonClick('6')}>6</button>
-          <button onClick={() => handleButtonClick('-')}>-</button>
-        </div>
-        <div className="row">
-          <button onClick={() => handleButtonClick('1')}>1</button>
-          <button onClick={() => handleButtonClick('2')}>2</button>
-          <button onClick={() => handleButtonClick('3')}>3</button>
-          <button onClick={() => handleButtonClick('*')}>*</button>
-        </div>
-        <div className="row">
-          <button onClick={() => handleButtonClick('0')}>0</button>
-          <button onClick={() => handleButtonClick('C')}>C</button>
-          <button onClick={() => handleButtonClick('=')}>=</button>
-          <button onClick={() => handleButtonClick('/')}>/</button>
-        </div>
+    <div className='App'>
+      <div className='container'>
+        <h1>Responsive Paragraph Word Counter</h1>
+        <textarea text={text} onChange={(e) => setText(e.target.value)}></textarea>
+        <p>Word Count: {count}</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
